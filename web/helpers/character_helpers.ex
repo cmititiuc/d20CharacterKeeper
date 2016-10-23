33de40bfc23f162fe84b2c2b2de9d4c9cc90a0b8
Cat <Catalin.Mititiuc@gmail.com>
2016-10-23 14:13:59 -0400

    refactor a long if statement on character form; remove commented out code

--- web/helpers/character_helpers.ex
+++ web/helpers/character_helpers.ex
@@ -6,4 +6,10 @@ defmodule D20CharacterKeeper.CharacterHelpers do
   def field_name(f) do
     (f.data.name || f.source.changes.name) |> String.slice(0..2) |> String.upcase
   end
+
+  def field_has_modifiers?(f) do
+    has_in_data?   = is_list(f.data.modifiers) && length(f.data.modifiers) != 0
+    has_in_source? = is_list(f.source.changes[:modifiers])
+    has_in_data? || has_in_source?
+  end
 end
